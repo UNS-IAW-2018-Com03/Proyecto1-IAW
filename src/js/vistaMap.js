065217850242d0487df4fa6dfc4be331ebcfbe8a
ManuelRios94 <you@example.com>
2018-04-16 23:17:38 -0300

    Fix divisor popUp

--- src/js/vistaMap.js
+++ src/js/vistaMap.js
@@ -27,6 +27,10 @@ function mostrar(arreglo){
 	var index;
 	for(index = 0; index < arreglo.length; ++index){
 		agregarComponente(arreglo[index],divGroup);
+		if(index !== arreglo.length-1){
+			agregarDivisor(divGroup)
+		}
+		
 	}
 	//Crea el popUP
 	$(panelReclamos).dialog();
@@ -53,11 +57,18 @@ function agregarComponente(componente,divContent){
 	
 	divReclamo.append(botonReclamo);
 	divReclamo.append(tituloReclamo);
-	var divisor = $("<div></div>").attr("class","box-content");
 	
 	divContent.append(divReclamo);
-	divContent.append(divisor);
 	
+	
+}
+
+/*
+Funcion que agrega un divisor a un componente
+*/
+function agregarDivisor(divContent){
+	var divisor = $("<div></div>").attr("class","box-content");
+	divContent.append(divisor);
 }
 
 /*
@@ -69,46 +80,44 @@ function ingresarDescripcion(componente){
 	$(panelReclamos).dialog("close");
 	//Remueve los hijso del panel de descripcion
 	var cell = document.getElementById("panelDescripcion");
-	if ( cell.hasChildNodes() ){
-		while ( cell.childNodes.length >= 1 ){
-			cell.removeChild( cell.firstChild );
-		}
+	if ( !cell.hasChildNodes() ){
+			
+		//Crear el contenido del popUP
+		var textArea = $("<textarea></textarea>").attr("class","form-control");
+		textArea.attr("type","textarea");
+		textArea.attr("name","message");
+		textArea.attr("id","txtDescripcion");
+		textArea.attr("placeholder","Escriba su mensaje aquí.");
+		textArea.attr("maxlength","500");
+		textArea.attr("rows","7");
+		var lblText = $("<label></label>").text("Escriba una descripcion de su reclamo (máx 500 caracteres)");
+		lblText.attr("for","name");
+		var divisor = $("<div></div>").attr("class","box-content");
+		var divGroup = $("<div></div>").attr("class","form-group");
+		divGroup.append(lblText);
+		divGroup.append(divisor);
+		divGroup.append(textArea);
+		var btnEnviar = $("<button></button>").text("Enviar Reclamo →");
+		btnEnviar.attr("type","submit");
+		btnEnviar.attr("class","btn btn-lg btn-success btn-block");
+		btnEnviar.click(function(){
+			guardarReclamo(componente, latitud, longitud);
+		});
+		var divBody	= $("<div></div>").attr("class","modal-body");
+		divBody.append(divGroup);
+		divBody.append(btnEnviar);
+		var titulo = $("<h4></h4>").text("Describa su Reclamo:");
+		titulo.attr("class","modal-title");
+		var divHeader = $("<div></div>").attr("class","modal-header");
+		divHeader.append(titulo);
+		var divContent = $("<div></div>").attr("class","modal-content");
+		divContent.append(divHeader);
+		divContent.append(divBody);
+		
+		$(panelDescripcion).append(divContent);
+		//Crear el popUp
+		$(panelDescripcion).dialog();
+		//Muestra el popUp
+		$(panelDescripcion).show();
 	}
-	//Crear el contenido del popUP
-	var textArea = $("<textarea></textarea>").attr("class","form-control");
-	textArea.attr("type","textarea");
-	textArea.attr("name","message");
-	textArea.attr("id","txtDescripcion");
-	textArea.attr("placeholder","Escriba su mensaje aquí.");
-	textArea.attr("maxlength","500");
-	textArea.attr("rows","7");
-	var lblText = $("<label></label>").text("Escriba una descripcion de su reclamo (máx 500 caracteres)");
-	lblText.attr("for","name");
-	var divisor = $("<div></div>").attr("class","box-content");
-	var divGroup = $("<div></div>").attr("class","form-group");
-	divGroup.append(lblText);
-	divGroup.append(divisor);
-	divGroup.append(textArea);
-	var btnEnviar = $("<button></button>").text("Enviar Reclamo →");
-	btnEnviar.attr("type","submit");
-	btnEnviar.attr("class","btn btn-lg btn-success btn-block");
-	btnEnviar.click(function(){
-		guardarReclamo(componente, latitud, longitud);
-	});
-	var divBody	= $("<div></div>").attr("class","modal-body");
-	divBody.append(divGroup);
-	divBody.append(btnEnviar);
-	var titulo = $("<h4></h4>").text("Describa su Reclamo:");
-	titulo.attr("class","modal-title");
-	var divHeader = $("<div></div>").attr("class","modal-header");
-	divHeader.append(titulo);
-	var divContent = $("<div></div>").attr("class","modal-content");
-	divContent.append(divHeader);
-	divContent.append(divBody);
-	
-	$(panelDescripcion).append(divContent);
-	//Crear el popUp
-	$(panelDescripcion).dialog();
-	//Muestra el popUp
-	$(panelDescripcion).show();
 }
